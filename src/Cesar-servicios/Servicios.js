e8112fa6021954bb10bd3b4978f360026b9b638d
Cesar <cesarluis4056@gmail.com>
2024-10-02 11:18:27 -0500

    fix: :bug: fixing-the-gallery

--- src/Cesar-servicios/Servicios.js
+++ src/Cesar-servicios/Servicios.js
@@ -2,11 +2,10 @@ import { useState } from "react";
 import { ServBanner } from "./components/ServBanner.js";
 import { ServCard } from "./components/ServCard.js";
 import { ServModal } from "./components/ServModal.js";
-import { ServGallery } from "./components/ServGallery.js"
+import { ServGallery } from "./components/ServGallery.js";
 import imagen1 from "./img-1.png";
 import imagen2 from "./img-2.png";
 import imagen3 from "./img-3.png";
-import img9 from "./img-9.jpeg";
 import img10 from "./img-10.jpeg";
 import img12 from "./img-12.jpeg";
 import img13 from "./img-13.jpeg";
@@ -41,22 +40,40 @@ const Servicios = () => {
   return (
     <div className="servicios">
       <ServBanner />
-      <div className="gallery">
+      <div className="servicios-info">
         <ServCard
           imgSrc={imagen1}
           title="Servicios Integrales de Monitoreo y Ensayos Ambientales"
-          onOpenModal={() => openModal([img15, img17, img16], "Servicios Integrales de Monitoreo y Ensayos Ambientales", "Realización de pruebas y análisis en laboratorio para evaluar la capacidad ambiental.")}
+          onOpenModal={() =>
+            openModal(
+              [img15, img17, img16],
+              "Servicios Integrales de Monitoreo y Ensayos Ambientales",
+              "Realización de pruebas y análisis en laboratorio para evaluar la capacidad ambiental."
+            )
+          }
         />
         <ServCard
           imgSrc={imagen2}
           title="Diseño de Reactores para Tratamiento de Aguas Residuales"
-          onOpenModal={() => openModal([img12, img14], "Diseño de Reactores para Tratamiento de Aguas Residuales", "Desarrollo de reactores a nivel laboratorio y piloto para el tratamiento de aguas.")}
+          onOpenModal={() =>
+            openModal(
+              [img12, img14],
+              "Diseño de Reactores para Tratamiento de Aguas Residuales",
+              "Desarrollo de reactores a nivel laboratorio y piloto para el tratamiento de aguas."
+            )
+          }
         />
+
         <ServCard
           imgSrc={imagen3}
           title="Asesoramiento de Trabajos de Investigación y Postgrado"
-          onOpenModal={() => openModal([img13, img10], "Asesoramiento de Trabajos de Investigación y Postgrado", "Apoyo especializado para la elaboración de tesinas y tesis de pregrado y postgrado.")}
-          
+          onOpenModal={() =>
+            openModal(
+              [img13, img10],
+              "Asesoramiento de Trabajos de Investigación y Postgrado",
+              "Apoyo especializado para la elaboración de tesinas y tesis de pregrado y postgrado."
+            )
+          }
         />
       </div>
       <ServModal
@@ -66,7 +83,7 @@ const Servicios = () => {
         closing={modalClosing}
       />
 
-      <ServGallery/>
+      <ServGallery />
     </div>
   );
 };
