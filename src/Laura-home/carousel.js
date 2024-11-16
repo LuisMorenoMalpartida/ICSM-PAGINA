03ea78b4596ec8cc83b43832fd89b79187e1603f
Jheyner <jheyner.misael@gmail.com>
2024-11-15 19:28:24 -0500

    imagenes de carrusel agregadas

--- src/Laura-home/carousel.js
+++ src/Laura-home/carousel.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import slide1 from './carrusel-1.jpeg';
-import slide2 from './carrusel-2.jpg';
+import slide1 from './carrusel-1b.jpeg';
+import slide2 from './carrusel-2b.jpg';
 import slide3 from './carrusel-3.jpg';
+import slide4 from './imagenes/img10.jpeg';
+import slide5 from './imagenes/img11b.jpeg';
 import './carousel.css';
 
 const Carousel = () => {
@@ -34,6 +36,18 @@ const Carousel = () => {
           data-bs-slide-to="2"
           aria-label="Slide 3"
         ></button>
+         <button
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide-to="3"
+          aria-label="Slide 4"
+        ></button>
+         <button
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide-to="4"
+          aria-label="Slide 5"
+        ></button>
       </div>
       <div className="carousel-inner">
         <div className="carousel-item active">
@@ -45,6 +59,21 @@ const Carousel = () => {
             />
           </Link>
         </div>
+        <div className="carousel-item">
+          <img
+            src={slide4}
+            alt="CIENCIA EN EL CAMPO"
+            className="d-block w-100"
+          />
+        </div>
+        <div className="carousel-item">
+          <img
+            src={slide5}
+            alt="CIENCIA EN EL CAMPO"
+            className="d-block w-100"
+          />
+        </div>
+      
         <div className="carousel-item">
           <img
             src={slide2}
